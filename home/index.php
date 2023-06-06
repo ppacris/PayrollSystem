0955f81fe9b012548c0f1935ee636ef93664c280
ppacris <arnnielkhenethpacris.cgc@gmail.com>
2023-06-06 09:46:33 +0800

    Update

--- home/index.php
+++ home/index.php
@@ -1,13 +1,19 @@
 <?php 
 require_once ("../headFoot/up.php");
 ?>
+<link rel="stylesheet" href="style.css">
 
-<div class="container">
-	<center>
-		<h1>Hello World!</h1>
-	</center>
+<div class="container position-absolute top-50 start-50 translate-middle">
+    <div class="display-date">
+        <span id="day">day</span>,
+        <span id="daynum">00</span>
+        <span id="month">month</span>
+        <span id="year">0000</span>
+    </div>
+    <div class="display-time"></div>
 </div>
 
+<script src="time.js"></script>
 <?php 
 require_once ("../headFoot/down.php");
 ?>
